a98b7ee105848b688306baaee7c2f57c4a647808
Meredith Thomas <meredityman@gmail.com>
2020-11-18 18:12:55 +0100

    First Commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import subprocess
+
+STELLARIUM_EXE = "C:\Program Files\Stellarium\stellarium.exe"
+
+
+
+
+def main():
+
+    subprocess.run("exit 1", shell=True, check=True)
+
+    pass
+
+
+
+
+if __name__ == "__main__":
+    main()
